215180f384fa80eae45899b5e5dcf6c2d2278427
unknown <markkim860@gmail.com>
2025-11-19 09:56:59 +0100

    new changes

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -2,9 +2,9 @@ import { LoginForm } from "../components/LoginForm";
 import { NavbarSecond } from "../components/NavbarSecond";
 export const LoginPage = () => {
   return (
-    <div className="w-screen h-screen bg-bgColor">
+    <div className="w-screen h-screen bg-bgSecond">
       <NavbarSecond />
-      <div className="w-screen h-screen flex items-center justify-center bg-bgColor">
+      <div className="w-screen h-screen flex items-center justify-center bg-bgSecond">
         <LoginForm />
       </div>
     </div>
--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -2,9 +2,9 @@ import { RegisterForm } from "../components/RegisterForm";
 import { NavbarSecond } from "../components/NavbarSecond";
 export const RegisterPage = () => {
   return (
-    <div className="w-screen h-screen bg-bgColor">
+    <div className="w-screen h-screen bg-bgSecond">
       <NavbarSecond />
-      <div className="w-screen h-screen flex items-center justify-center bg-bgColor">
+      <div className="w-screen h-screen flex items-center justify-center bg-bgSecond">
         <RegisterForm />
       </div>
     </div>
